4790de3421368aa71ef5ed36aa5a52751d56bcae
Adam Kerr <ajrkerr@gmail.com>
2015-05-31 23:20:00 -0400

    Re-adds pixel flame

--- pixel_master_dumb/animation_flame.h
+++ pixel_master_dumb/animation_flame.h
@@ -10,6 +10,7 @@ class AnimationFlame : public Animation {
   RGBConverter conv;
   double *flameDirection;
   double *flameDelta;
+  Pixel pixel;
 
 public:
   ~AnimationFlame() {
@@ -30,22 +31,39 @@ public:
   }
 
   void loop(unsigned long currentFrame, PixelStripConfig config, PixelStrip *strip) {
-    Pixel pixel;
     
-    double hue;
-    double sat = 1.0;
-    double val = 1.0; 
-  
-    RGBConverter conv; // Convert from HSV to RGB space
-    byte rgb[3];
-  
-    for(int i = 0; i < config.numPixels; i++) {
-      int colorPos = i + currentFrame;
-      int framesForCycle = 250;
-      hue = ((double)(colorPos % framesForCycle) / framesForCycle);
+    double primaryHSV[3];
+    conv.rgbToHsv(config.primaryColor.red, config.primaryColor.green, config.primaryColor.blue, primaryHSV);
+    double primaryHue = primaryHSV[0];
+    double primarySaturation = primaryHSV[1];
+    double primaryValue = primaryHSV[2];
 
-      conv.hsvToRgb(hue, sat, val, rgb);
-      
+    double maxVariance = 0.08;
+    double flameStep = 0.00085;
+    int entropy = 07;
+
+    byte rgb[3];
+
+    for(int i = 0; i < config.numPixels; i++) { 
+      if(random(100) < entropy) {
+        flameDirection[i] = flameDirection[i] * -1;
+      }
+
+      flameDelta[i] += flameDirection[i] * flameStep;
+
+      if(flameDelta[i] > maxVariance) {
+        flameDirection[i] = -1;
+        flameDelta[i] = maxVariance;
+      } else if((0 - flameDelta[i]) < (0 - maxVariance)) {
+        flameDirection[i] = 1;
+        flameDelta[i] = 0 - maxVariance;
+      }
+
+      double hue = constrain(primaryHue + flameDelta[i], 0.0, 1.0);
+      double saturation = primarySaturation;
+      double value = constrain(primaryValue + flameDelta[i], 0.0, 1.0);
+
+      conv.hsvToRgb(hue, saturation, value, rgb);
       pixel.red = rgb[0];
       pixel.green = rgb[1];
       pixel.blue = rgb[2];
